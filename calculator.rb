ff72a6e3c1a25c4be456faf68e0ba3ae98767a3c
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-23 14:41:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,8 @@ first_number = get_variable_from_file('./calculator.rb', "first_number")
 
     expect(first_number).to be_an(Integer).or be_a(Float)
   end
+  second_number = get_variable_from_file('./calculator.rb', "second_number")
+
+    expect(second_number).to be_an(Integer).or be_a(Float)
+    expect(second_number).not_to equal(0)
+  end
