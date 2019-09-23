39671aee6859ece149d19a9561631cc2a22dd3f3
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-23 14:28:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,3 @@
 first_number  = get_variable_from_file('./calculator.rb', "first_number")
 second_number = get_variable_from_file('./calculator.rb', "second_number")
-expect(sum) 
+expect(sum).to eq(first_number+second_number) 
